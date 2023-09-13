f500be0d5735b39df33bb7bb3af07d05d6a7a5bf
Camelia03 <camelia_weber@yahoo.com>
2023-09-13 15:34:31 +0000

    Updating the state - challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import StatefulGreeting from './components/StatefulGreeting';
+import StatefulGreetingWithCallback from './components/StatefulGreeting WithCallback';
 
 function App() {
   return (
     <div className="App">
-      <StatefulGreeting greeting="I'm a stateful class component!"  />
+      <StatefulGreetingWithCallback />
     </div>
   );
 }
