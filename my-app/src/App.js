447151862c2dc9e13773187cdbd2ca15e2e7fc0f
Camelia03 <camelia_weber@yahoo.com>
2023-09-20 14:58:32 +0000

    call useEffect on every render

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-import UseStateWithArrays from './components/UseStateWithArrays';
+import "./App.css";
+import ClassCounter from "./components/ClassCounter";
+import UseEffectCounter from "./components/UseEffectCounter";
 
 function App() {
-  return (
-    <div className="App">
-      <UseStateWithArrays />
-    </div>
-  );
+  return <div className="App">
+      {/* <ClassCounter /> */}
+      <UseEffectCounter />
+  </div>;
 }
 
 export default App;
